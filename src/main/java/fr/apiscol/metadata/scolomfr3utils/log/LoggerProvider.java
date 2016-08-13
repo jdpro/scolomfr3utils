660d737e27e4d9bb208179a499264796c7258379
Joachim Dornbusch <joachim.dornbusch@agroparistech.fr>
2016-08-13 08:03:25 +0200

    Fixed : twice logging issues

--- src/main/java/fr/apiscol/metadata/scolomfr3utils/log/LoggerProvider.java
+++ src/main/java/fr/apiscol/metadata/scolomfr3utils/log/LoggerProvider.java
@@ -1,11 +1,43 @@
 package fr.apiscol.metadata.scolomfr3utils.log;
 
+import java.util.Enumeration;
+
+import org.apache.log4j.Appender;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class LoggerProvider {
+
+
 	public static Logger getLogger(Class<?> callingClass) {
-		BasicConfigurator.configure();
-		return Logger.getLogger(callingClass);
+		if (!log4JisConfigured()) {
+			BasicConfigurator.configure();
+		}
+
+		Logger logger = Logger.getLogger(callingClass);
+		return logger;
 	}
+
+	/**
+	 * Returns true if it appears that log4j have been previously configured.
+	 * This code checks to see if there are any appenders defined for log4j
+	 * which is the definitive way to tell if log4j is already initialized
+	 */
+	public static boolean log4JisConfigured() {
+		Enumeration<Appender> appenders = Logger.getRootLogger().getAllAppenders();
+		if (appenders.hasMoreElements()) {
+			return true;
+		} else {
+			@SuppressWarnings("unchecked")
+			Enumeration<Appender> loggers = LogManager.getCurrentLoggers();
+			while (loggers.hasMoreElements()) {
+				Logger c = (Logger) loggers.nextElement();
+				if (c.getAllAppenders().hasMoreElements())
+					return true;
+			}
+		}
+		return false;
+	}
+
 }
