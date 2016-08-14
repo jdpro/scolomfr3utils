10dc0ab2a9894ef0c525071a024487a4ce009fe7
Joachim Dornbusch <joachim.dornbusch@agroparistech.fr>
2016-08-14 08:23:43 +0200

    Tests refactoring

---
+++ src/test/java/fr/apiscol/metadata/scolomfr3utils/XsdValidationITest.java
@@ -0,0 +1,53 @@
+package fr.apiscol.metadata.scolomfr3utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.apiscol.metadata.scolomfr3utils.Scolomfr3Utils;
+
+/**
+ * Test xsd validation with different kinds of files
+ */
+public class XsdValidationITest {
+	private static final String MISSING_SCOLOMFR_FILE_FAILURE_MESSAGE = "Please provide a scolomfr file before calling scolomfrutils methods.";
+	private static final String DUPLICATE_GENERAL_ELEMENT_FAILURE_MESSAGE = "cvc-identity-constraint.4.1: Duplicate unique value [general] declared for identity constraint \"lomUnique\" of element \"lom\".";
+	private Scolomfr3Utils scolomfrutils;
+
+	@Before
+	public void setup() {
+		scolomfrutils = new Scolomfr3Utils();
+		scolomfrutils.setScolomfrVersion("3.0");
+	}
+
+	@Test
+	public void testXsdValidationSuccess() {
+		File scolomfrFile = new File("src/test/data/3.0/valid/exemple.xml");
+		scolomfrutils.setScolomfrFile(scolomfrFile);
+		scolomfrutils.checkXsd();
+		assertTrue("There should be no validation messages", scolomfrutils.getMessages().isEmpty());
+		assertTrue("Result of validation with the official exemple file should be valid", scolomfrutils.isValid());
+	}
+
+	@Test
+	public void testXsdValidationFailure() {
+		File scolomfrFile = new File("src/test/data/3.0/invalid/double-general.xml");
+		scolomfrutils.setScolomfrFile(scolomfrFile);
+		scolomfrutils.checkXsd();
+		assertTrue("The validation messages should contain : " + DUPLICATE_GENERAL_ELEMENT_FAILURE_MESSAGE,
+				scolomfrutils.getMessages().contains(DUPLICATE_GENERAL_ELEMENT_FAILURE_MESSAGE));
+		assertFalse("Result of validation with duplicate general element should not be valid", scolomfrutils.isValid());
+	}
+
+	@Test
+	public void testXsdWithoutFile() {
+		scolomfrutils.checkXsd();
+		assertTrue("The validation messages should contain : " + MISSING_SCOLOMFR_FILE_FAILURE_MESSAGE,
+				scolomfrutils.getMessages().contains(MISSING_SCOLOMFR_FILE_FAILURE_MESSAGE));
+		assertFalse("Result of validation is valid", scolomfrutils.isValid());
+	}
+}
